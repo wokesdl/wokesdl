d549c564c478cbd21d3c4501e5f78d657c7fc584
wokesdl <wokesdl3@gmail.com>
2025-03-02 00:03:24 +0000

    updated the store page of wokesdl

--- wokesdlPages/urls.py
+++ wokesdlPages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MainHome, Store, LookBook, AboutWokeSdl
+from .views import MainHome, Store, LookBook, AboutWokeSdl, WokeSdlHome
 
 urlpatterns =[
 path('',MainHome.as_view(),name='mainHome'),
 path('store/',Store.as_view(),name='store'),
 path('lookbook/<str:accessCode>/',LookBook.as_view(),name='lookbook'),
 path('about/',AboutWokeSdl.as_view(),name='about'),
+path('wokesdlhome/',WokeSdlHome.as_view(),name='wokesdlHome')
 ]
